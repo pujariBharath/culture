030fb5d28cfc1d526dd226f0d27d2baca9f52a6c
Bharath <pujaribharath77@gmail.com>
2024-12-26 10:38:15 +0530

    fxcgn

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,3 +7,14 @@ module.exports = {
   plugins: [],
 }
 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './pages/**/*.{html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
